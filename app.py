599d62d736a695cedf79fd96d3b2f6b17bbb1d3e
Luise Higuera <luisehica@gmail.com>
2021-06-03 12:17:17 -0500

    changing name of main file to app

--- app.py
+++ app.py
@@ -1,34 +1,44 @@
-import dash
+# Import dash 
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-import flask
-from utils import Header
 
-### Setup dash plotly
-external_stylesheets = [dbc.themes.BOOTSTRAP]
-#server = flask.Flask(__name__)
+# Import plotly
+import plotly.graph_objects as go
+
+# Import dependencies for callbacks
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 
-app = dash.Dash(__name__, 
-    suppress_callback_exceptions=True,
-    external_stylesheets=external_stylesheets,
-    meta_tags=[{'name': 'viewport',
-        'content': 'width=device-width'}] #, initial-scale=1.0'}]
-    )
-    
-server = app.server
-app.title = 'Beek Dashboard Data Challenge'
+# Connect to main app.py file, apps pages and backend
+from config import app, create_layout
+from apps import main
 
-### Create Layout
-def create_layout(app, page):
-    layout = html.Div(
-        [
-            Header(app),
-            dbc.Container(
-                [
-                    page.content(app)
-                ]
-            )
-        ]
-    )
-    return layout
+
+
+
+# Describe the layout/UI of the app
+app.layout = html.Div(
+    [
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="page-content")
+    ]
+)
+
+
+
+### Callback for page salection
+@app.callback(Output(component_id='page-content', component_property='children'),
+    Input(component_id='url', component_property='pathname'))
+def display_page(pathname):
+    if pathname == '/help':
+        return create_layout(app, help)
+    else:
+        return create_layout(app, main)
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0' ,port=8080)
