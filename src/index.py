424d708e8dac554ea70473ca5a359a97ddad4e68
Luise Higuera <luisehica@gmail.com>
2021-06-03 11:06:54 -0500

    a litle change

--- src/index.py
+++ src/index.py
@@ -16,8 +16,6 @@ from app import app, create_layout
 from apps import main
 
 
-from datetime import timedelta
-
 
 
 # Describe the layout/UI of the app
@@ -43,4 +41,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0',debug=True ,port=8080)
+    app.run_server(host='0.0.0.0' ,port=8080)
